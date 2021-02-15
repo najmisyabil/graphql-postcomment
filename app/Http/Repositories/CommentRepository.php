085d77baa1c2ff95325d4e1ce00e8575e2dd65cc
Syabil <najmisyabil@gmail.com>
2021-02-15 18:57:35 +0800

    new Repository for Posts and Comments

---
+++ app/Http/Repositories/CommentRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Repositories;
+
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
+class CommentRepository
+{
+    protected $url;
+
+    /**
+     * Initialize main url
+     */
+    public function __construct()
+    {
+        $this->url = 'https://jsonplaceholder.typicode.com/comments';
+    }
+
+    public function get()
+    {
+        return $this->tryGet($this->url);
+    }
+
+    private function tryGet(string $url)
+    {
+        try {
+            $response = Http::get($url)->json();
+        } catch (\Exception $e) {
+            Log::error('Failed to fetch Comments', ['message' => $e->getMessage()]);
+
+            $response = 400;
+        }
+
+        return $response;
+    }
+}
---
+++ app/Http/Repositories/PostRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Repositories;
+
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
+class PostRepository
+{
+    protected $url;
+
+    /**
+     * Initialize main url
+     */
+    public function __construct()
+    {
+        $this->url = 'https://jsonplaceholder.typicode.com/posts';
+    }
+
+    public function get()
+    {
+        return $this->tryGet($this->url);
+    }
+
+    private function tryGet(string $url)
+    {
+        try {
+            $response = Http::get($url)->json();
+        } catch (\Exception $e) {
+            Log::error('Failed to fetch Posts', ['message' => $e->getMessage()]);
+
+            $response = 400;
+        }
+
+        return $response;
+    }
+}
